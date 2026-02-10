405eb1aff04eb1c3a3bf584ce4e0d89dbf47a447
Hotiovip <hotiovipita@gmail.com>
2026-02-10 18:36:51 +0100

    Added netherite_diamond_golden_pickaxe

--- src/client/java/org/hotiovip/recipe/HToolsRecipeProvider.java
+++ src/client/java/org/hotiovip/recipe/HToolsRecipeProvider.java
@@ -7,12 +7,16 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
 import net.minecraft.world.item.Item;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.hotiovip.HToolsItems;
+
+import static net.minecraft.data.recipes.SmithingTransformRecipeBuilder.smithing;
 
 public class HToolsRecipeProvider extends FabricRecipeProvider {
     public HToolsRecipeProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
@@ -26,7 +30,7 @@ public class HToolsRecipeProvider extends FabricRecipeProvider {
             public void buildRecipes() {
                 HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
 
-                shaped(RecipeCategory.MISC, HToolsItems.DIAMOND_GOLDEN_PICKAXE, 1)
+                shaped(RecipeCategory.TOOLS, HToolsItems.DIAMOND_GOLDEN_PICKAXE, 1)
                         .pattern(" d ")
                         .pattern("dgd")
                         .define('d', Items.DIAMOND)
@@ -35,6 +39,15 @@ public class HToolsRecipeProvider extends FabricRecipeProvider {
                         .unlockedBy(getHasName(Items.GOLD_INGOT), has(Items.GOLD_INGOT))
                         .unlockedBy(getHasName(Items.DIAMOND), has(Items.DIAMOND))
                         .save(output);
+
+                smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE),
+                        Ingredient.of(HToolsItems.DIAMOND_GOLDEN_PICKAXE),
+                        Ingredient.of(Items.NETHERITE_INGOT),
+                        RecipeCategory.TOOLS,
+                        HToolsItems.NETHERITE_DIAMOND_GOLDEN_PICKAXE)
+                        .unlocks(getHasName(Items.NETHERITE_INGOT), has(Items.NETHERITE_INGOT))
+                        .unlocks(getHasName(HToolsItems.DIAMOND_GOLDEN_PICKAXE), has(HToolsItems.DIAMOND_GOLDEN_PICKAXE))
+                        .save(output, "netherite_diamond_golden_pickaxe");
             }
         };
     }
--- src/main/java/org/hotiovip/HToolsItems.java
+++ src/main/java/org/hotiovip/HToolsItems.java
@@ -30,6 +30,20 @@ public class HToolsItems {
             new Item.Properties().pickaxe(DIAMOND_GOLDEN_TOOL_MATERIAL, 1.0F, -2.8F)
     );
 
+    public static final TagKey<Item> REPAIRS_NETHERITE_DIAMOND_GOLDEN_ARMOR = TagKey.create(BuiltInRegistries.ITEM.key(), Identifier.fromNamespaceAndPath(HTools.MOD_ID, "repairs_netherite_diamond_golden_armor"));
+    public static final ToolMaterial NETHERITE_DIAMOND_GOLDEN_TOOL_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_NETHERITE_TOOL,
+            ToolMaterial.NETHERITE.durability(),
+            ToolMaterial.GOLD.speed(),
+            ToolMaterial.GOLD.attackDamageBonus(),
+            ToolMaterial.GOLD.enchantmentValue(),
+            REPAIRS_NETHERITE_DIAMOND_GOLDEN_ARMOR
+    );
+    public static final Item NETHERITE_DIAMOND_GOLDEN_PICKAXE = register(
+            "netherite_diamond_golden_pickaxe",
+            Item::new,
+            new Item.Properties().pickaxe(NETHERITE_DIAMOND_GOLDEN_TOOL_MATERIAL, 1.0F, -2.8F)
+    );
 
     public static <GenericItem extends Item> GenericItem register(String name, Function<Item.Properties, GenericItem> itemFactory, Item.Properties settings) {
         // Create the item key.
@@ -45,7 +59,11 @@ public class HToolsItems {
     }
 
     public static void initialize() {
+        // Add items to creative inventory
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
                 .register((itemGroup) -> itemGroup.accept(DIAMOND_GOLDEN_PICKAXE));
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
+                .register((itemGroup) -> itemGroup.accept(NETHERITE_DIAMOND_GOLDEN_PICKAXE));
     }
 }
